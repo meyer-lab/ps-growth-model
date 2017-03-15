6e39edbf477fd284dfe133ae5f0f7c2ba67fd0df
Colton Stearns <colton.stearns2@gmail.com>
2017-03-15 14:55:33 -0400

    Fixed formatting
    Fixed formatting between old version that I worked with and new version
    that Minyi had already uploaded

--- grmodel/GrowthModel.py
+++ grmodel/GrowthModel.py
@@ -3,9 +3,7 @@ from numpy import arange
 from pylab import plot, figure, xlabel, ylabel, legend, show
 import matplotlib.patches as mpatches
 
-# to do: 1. mc gives params in a list not list of lists, 2. don't call funciton each time
-#for every value a,b,c,e... 3. put rate function in here --> cleaner
-#4. unit test 5. get this to work 
+
 class GrowthModel(object):
     
     #calculates rate at the given time
@@ -16,7 +14,7 @@ class GrowthModel(object):
             poly_sum = 0
             for i in range(len(rate_equation)):
                 poly_sum += rate_equation[i]*time**i
-            poly_sum = exp(poly_sum) #change this??
+            poly_sum = exp(poly_sum)
             list_of_rates.append(poly_sum)
         return list_of_rates
 
@@ -58,7 +56,7 @@ class GrowthModel(object):
 	#params:
 	#params	list of parameters for model (a, b, c, d, e)
 	#t_interval 	time interval over which to solve the function
-<<<<<<< HEAD
+
 	#y0 	list with the initial values for each state 
     def simulate(self, params, t_interval, y0):
     		out = odeint(self.ODEfun, y0, t_interval, args = (params,))
@@ -66,30 +64,20 @@ class GrowthModel(object):
 
 	#plots the results from a simulation
 	#if animate is True then the line plot over time 
-    def plotSimulation(self, state, t_interval, animate):
-    		fig = figure()
+	#plots the results from a simulation
+	#if animate is True then the line plot over time
+    def plotSimulation(self, state, t_interval):
+    		figure()
     		xlabel('Time')
     		ylabel('Number of Cells')
-    		if animate:
-    			plot(t_interval, state[:, 0], 'b-', alpha = 0.2, label = "live")
-    			plot(t_interval, state[:, 1], 'r-', alpha = 0.2,label = "dead")
-    			plot(t_interval, state[:, 2], 'g-', alpha = 0.2,label = "early apoptosis")
-    			plot(t_interval, state[:, 3], 'k-', alpha = 0.2,label = "gone")
-    		else:
-    			plot(t_interval, state[:, 0], 'b-', label = "live")
-    			plot(t_interval, state[:, 1], 'r-', label = "dead")
-    			plot(t_interval, state[:, 2], 'g-', label = "early apoptosis")
-    			plot(t_interval, state[:, 3], 'k-', label = "gone")
-    		legend(loc='upper right')
-    		#show()
-    		if animate:
-    			def animate(i):
-    			     plot(t_interval[0:i], state[0:i,0], 'b-')
-    			     plot(t_interval[0:i], state[0:i,1], 'r-')
-    			     plot(t_interval[0:i], state[0:i,2], 'g-')
-    			     plot(t_interval[0:i], state[0:i,3], 'k-')
     
-    			ani = animation.FuncAnimation(fig, animate, interval=1)
+    		plot(t_interval, state[:, 0], 'b-', label="live")
+    		plot(t_interval, state[:, 1], 'r-', label="dead")
+    		plot(t_interval, state[:, 2], 'g-', label="early apoptosis")
+    		plot(t_interval, state[:, 3], 'k-', label="gone")
+    
+    		legend(loc='upper right')
+    
     		show()
 
 
@@ -97,36 +85,5 @@ if __name__ == '__main__':
     t = arange(0, 10, 0.005)
     init_state = [10000, 0, 0, 0]
     params = GrowthModel.mcFormat([.0009, -.016, .01, .008, .0007, .005, -.001,-.0071, .0008, .005])
-    #will turn the list above into what mc gives us
     out = GrowthModel().simulate(params, t, init_state)
-    GrowthModel().plotSimulation(out, t, True)
-=======
-	#y0 	list with the initial values for each state
-	def simulate(self, params, t_interval, y0):
-		out = odeint(self.ODEfun, y0, t_interval, args=tuple(params))
-		return out
-
-	#plots the results from a simulation
-	#if animate is True then the line plot over time
-	def plotSimulation(self, state, t_interval):
-		figure()
-		xlabel('Time')
-		ylabel('Number of Cells')
-
-		plot(t_interval, state[:, 0], 'b-', label="live")
-		plot(t_interval, state[:, 1], 'r-', label="dead")
-		plot(t_interval, state[:, 2], 'g-', label="early apoptosis")
-		plot(t_interval, state[:, 3], 'k-', label="gone")
-
-		legend(loc='upper right')
-
-		show()
-
-
-if __name__ == '__main__':
-	t = arange(0, 200, 0.2)
-	init_state = [10000, 0, 0, 0]
-	params = [lambda t: 0.2, lambda t: 0.2, lambda t: 0.1, lambda t: 0.7, lambda t: .5]
-	out = GrowthModel().simulate(params, t, init_state)
-	GrowthModel().plotSimulation(out, t)
->>>>>>> origin/master
+    GrowthModel().plotSimulation(out, t)
