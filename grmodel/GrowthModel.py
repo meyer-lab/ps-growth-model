9e4fa69166323ecd0bdf9a44b27182efa57187c5
Aaron Meyer <a@ameyer.me>
2017-06-13 08:40:06 -0400

    Heavy linting.

--- grmodel/GrowthModel.py
+++ grmodel/GrowthModel.py
@@ -2,16 +2,18 @@ import numpy as np
 
 np.seterr(over='raise')
 
+
 def rate_values(parameters, time):
     '''Calculates the rates of each cellular process at a given time
 
     Returns a list of values of e raised to the power of teh calculated rate
     (for each set of parameters) as to ensure no negative value. The input is
-    a list of lists, each index of the inner list a coeficient of time to the power
-    of that index. The outer list represents the number of functions.
+    a list of lists, each index of the inner list a coeficient of time to the
+    power of that index. The outer list represents the number of functions.
 
     Arguments:
-        parameters (list of lists): contains number of functions and parameters of each funciton
+        parameters (list of lists): contains number of functions and
+            parameters of each function
         time (float): time at which rate is calculated
 
     Returns:
@@ -21,14 +23,19 @@ def rate_values(parameters, time):
     if time < 0:
         raise ValueError
 
-    tt = np.power(time, np.arange(parameters.shape[0], 0, -1, dtype=np.float64)).T
+    tt = np.power(time,
+                  np.arange(parameters.shape[0], 0, -1, dtype=np.float64)).T
 
     return np.exp(np.matmul(parameters.T, tt))
 
+
 def logpdf_sum(x, loc, scale):
-    """ Calculate the likelihood of an observation applied to a normal distribution. """
-    prefactor = - np.log(scale * np.sqrt(2*np.pi))
-    summand = -np.square((x - loc)/(np.sqrt(2) * scale))
+    """
+    Calculate the likelihood of an observation applied to a
+    normal distribution.
+    """
+    prefactor = - np.log(scale * np.sqrt(2 * np.pi))
+    summand = -np.square((x - loc) / (np.sqrt(2) * scale))
     return prefactor + summand
 
 
@@ -37,8 +44,9 @@ def ODEfun(state, t, params):
     ODE function of cells living/dying/undergoing early apoptosis
 
     params:
-    state	the number of cells in a particular state (LIVE, DEAD, EARLY_APOPTOSIS)
-    t 	time
+    state   the number of cells in a particular state
+            (LIVE, DEAD, EARLY_APOPTOSIS)
+    t   time
     a 	parameter between LIVE -> LIVE (cell division)
     b 	parameter between LIVE -> DEAD
     c 	parameter between LIVE -> EARLY_APOPTOSIS
@@ -54,9 +62,9 @@ def ODEfun(state, t, params):
 
     rates = rate_values(params, t)
 
-    return np.array([rates[0]*LIVE - rates[1]*LIVE - rates[2]*LIVE,
-                     rates[1]*LIVE - rates[4]*DEAD + rates[3]*EARLY_APOPTOSIS,
-                     rates[2]*LIVE - rates[3]*EARLY_APOPTOSIS], dtype=np.float64)
+    return np.array([(rates[0] - rates[1] - rates[2]) * LIVE,
+                     rates[1] * LIVE - rates[4] * DEAD + rates[3] * EARLY_APOPTOSIS,
+                     rates[2] * LIVE - rates[3] * EARLY_APOPTOSIS], dtype=np.float64)
 
 
 def mcFormat(mcParams):
@@ -65,6 +73,7 @@ def mcFormat(mcParams):
         raise ValueError("Length of mcParams must be a multiple of 5.")
 
     return np.reshape(mcParams, (-1, 5), order='F')
+
 
 def simulate(params, t_interval):
     """
@@ -92,10 +101,12 @@ def simulate(params, t_interval):
 
     return (t_interval, out)
 
+
 def paramsWithinLimits(params, t_int, maxVal):
     """
-    This only checks that the parameters don't pass through the bound in the duration.
-    It's still possible they start too high, which is handled by the lower and upper
+    This only checks that the parameters don't pass through
+    the bound in the duration. It's still possible they start
+    too high, which is handled by the lower and upper
     bounds hopefully.
     """
 
@@ -119,41 +130,44 @@ def paramsWithinLimits(params, t_int, maxVal):
 
 
 class GrowthModel:
+
     def likelihood(self, confl_model, green_model, sigma_sim_live, sigma_sim_dead):
         """
         Calculates the log likelihood of a simulation given the experimental data.
 
         parameters:
         table_sim   merged table with the transformed simulation predictions
-        sigma_sim_live  the sigma used for the normal distribution for live cells
-        sigma_sim_dead  the sigma used for the normal distribution for dead cells
+        sigma_sim_live  the sigma used for the normal dist. for live cells
+        sigma_sim_dead  the sigma used for the normal dist. for dead cells
         """
 
         try:
-            logSqrErr  = np.sum(logpdf_sum(self.expTable[1], loc=confl_model, scale=sigma_sim_live))
+            logSqrErr = np.sum(logpdf_sum(self.expTable[1], loc=confl_model, scale=sigma_sim_live))
             logSqrErr += np.sum(logpdf_sum(self.expTable[2], loc=green_model, scale=sigma_sim_dead))
         except FloatingPointError:
             return -np.inf
 
         return logSqrErr
 
-
     def logL(self, paramV):
         """
-        Run simulation using paramV, and compare results to observations in self.selCol
+        Run simulation using paramV, and compare results to observations in
+        self.selCol
         """
 
         # Return -inf for parameters out of bounds
         if not np.all(np.isfinite(paramV)):
             return -np.inf
-        elif np.any(np.less(paramV, self.lb)) or np.any(np.greater(paramV, self.ub)):
+        elif np.any(np.less(paramV, self.lb)):
+            return -np.inf
+        elif np.any(np.greater(paramV, self.ub)):
             return -np.inf
 
         # Format parameters to list of lists (except last 4 entries)
         params = mcFormat(paramV[:-4])
 
         # Check that the parameter values are reasonable over the interval
-        if not paramsWithinLimits(params, (np.min(self.uniqueT), np.max(self.uniqueT)), 2.0):
+        if not paramsWithinLimits(params, self.tRange, 2.0):
             return -np.inf
 
         # Calculate model data table
@@ -165,13 +179,13 @@ class GrowthModel:
         # Power transform conversion constants
         paramV[-4:] = np.power(10, paramV[-4:])
 
-        #scale model data table with conversion constants
-        confl_model = paramV[-4]*np.interp(self.expTable[0], model[0], np.sum(model[1], axis=1))
-        green_model = paramV[-3]*np.interp(self.expTable[0], model[0], model[1][:, 1] + model[1][:, 2])
+        # Scale model data table with conversion constants
+        confl_mod = paramV[-4] * np.interp(self.expTable[0], model[0], np.sum(model[1], axis=1))
+        green_mod = paramV[-3] * np.interp(self.expTable[0], model[0], model[1][:, 1] + model[1][:, 2])
 
-        # Run likelihood function with modeled and experiemental data, with standard 
-        # deviation given by last two entries in paramV
-        likel = self.likelihood(confl_model, green_model, paramV[-2], paramV[-1])
+        # Run likelihood function with modeled and experiemental data, with
+        # standard deviation given by last two entries in paramV
+        likel = self.likelihood(confl_mod, green_mod, paramV[-2], paramV[-1])
 
         if np.isnan(likel) is True:
             print('Got a NaN which shouldn\'t happen.')
@@ -179,23 +193,25 @@ class GrowthModel:
 
         return likel
 
-
     def __init__(self, selCol, loadFile=None, complexity=2):
-        import os
-        import itertools
+        """ Initialize class. """
+        from os.path import join, dirname, abspath
+        import itertools as itt
         import pandas
 
         # If no filename is given use a default
         if loadFile is None:
             loadFile = "091916_H1299_cytotoxic_confluence"
 
-        # Find path for csv files, on any machine wherein the repository exists.
-        path = os.path.dirname(os.path.abspath(__file__))
+        # Find path for csv files in the repository.
+        pathcsv = join(dirname(abspath(__file__)), 'data/' + loadFile)
 
         # Read in both observation files. Return as formatted pandas tables.
         # Data tables to be kept within class.
-        data_confl = pandas.read_csv(os.path.join(path, ('data/' + loadFile + '_confl.csv')), infer_datetime_format=True)
-        data_green = pandas.read_csv(os.path.join(path, ('data/' + loadFile + '_green.csv')), infer_datetime_format=True)
+        data_confl = pandas.read_csv(pathcsv + '_confl.csv',
+                                     infer_datetime_format=True)
+        data_green = pandas.read_csv(pathcsv + '_green.csv',
+                                     infer_datetime_format=True)
 
         # Pull out selected column data
         self.selCol = selCol
@@ -207,10 +223,11 @@ class GrowthModel:
 
         # Match time range and interval to experimental time range and interval
         self.uniqueT = np.sort(np.unique(self.expTable[0]))
+        self.tRange = (np.min(self.uniqueT), np.max(self.uniqueT))
 
         # Parameter names
         ps = ['a', 'b', 'c', 'd', 'e']
-        self.pNames = list(itertools.chain.from_iterable(itertools.repeat(x, complexity) for x in ps))
+        self.pNames = list(itt.chain.from_iterable(itt.repeat(x, complexity) for x in ps))
         self.pNames = self.pNames + ['conv_confl', 'conv_green', 'err_confl', 'err_green']
 
         # Specify lower bounds on parameters (log space)
