040c6b0799b590f49cc4b77a6eba8ebbde106a73
nguan <ningguan@mit.edu>
2017-08-08 20:20:03 -0400

    Copy changes from FitCurves4

---
+++ grmodel/fitcurves.py
@@ -0,0 +1,128 @@
+import bz2
+import os
+import pymc3 as pm
+import numpy as np
+import scipy as sp
+import pandas as pd
+from .sampleAnalysis import readCols
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+class MultiDrugs:
+    def __init__(self, columns, drugs, fitparams):
+        self.columns = columns
+        self.drugs = drugs
+        self.fitparams = fitparams
+
+    def get_tables(self):
+        df = readCols(self.columns)[1]
+        params = self.fitparams[:]
+        params.append('Column')
+        params.append('Condition')
+        df = df.loc[:, params]
+        # Set up table for each drug
+        drugdict = dict()
+        for drug in self.drugs:
+            dfd = df[df['Condition'].str.contains(drug+' ')]
+            # Break if drug not in dataset
+            if dfd.empty:
+                print("Error: Drug not in dataset")
+                break
+
+            # Add dose to table
+            dfd = dfd.copy()
+            dfd[drug+'-dose'] = dfd.loc[:, 'Condition'].str.split(' ').str[1]
+            dfd.loc[:, drug+'-dose'] = pd.to_numeric(dfd[drug+'-dose'])
+            dfd[drug+'-logdose'] = np.log10(dfd.loc[:, drug+'-dose'])
+            
+            # Convert self.fitparams to ln space
+            for param in self.fitparams:
+                dfd.loc[:, param] = dfd[param].apply(np.log)
+            drugdict[drug] = dfd
+        return drugdict
+
+    def fitCurves(self):
+        drugdict = self.get_tables()
+        curvefits = dict()
+        for key in drugdict:
+            val = drugdict[key]
+            for param in self.fitparams:
+                fit = CurveFit(key, val, param)
+                fitmap = fit.sampling()
+                curvefits[str(key)+'-'+str(param)] = fitmap
+        self.curvefits = curvefits
+        return curvefits
+
+    def saveCurves(self, filePrefix):
+        if os.path.exists(filePrefix + '_curves.pkl'):
+            os.remove(filePrefix + '_curves.pkl')
+        
+        pickle.dump(self.curvefits, bz2.BZ2File(filePrefix + '_curves.pkl', 'w'))
+
+
+class CurveFit:
+    def __init__(self, drug, drugdf, fitparam): 
+        self.drug = drug
+        self.drugdf = drugdf
+        self.fitparam = fitparam
+
+    def get_dicts(self):
+        dosedata = dict()
+        kdes = dict()
+        drugdf = self.drugdf.copy()
+        logdoses = drugdf.loc[:,self.drug+'-logdose']
+        self.logdoses = list(logdoses.drop_duplicates(keep='first'))
+        for logdose in logdoses:
+            df = drugdf.loc[drugdf[self.drug+'-logdose'] == logdose]
+            kde = sp.stats.gaussian_kde(np.exp(df.loc[:, self.fitparam]))
+            dosedata[logdose] = df.loc[:, self.fitparam]
+            kdes[logdose] = kde
+        return (dosedata, kdes)
+
+    def build_model(self):
+
+        curve_model = pm.Model()
+        with curve_model:
+            dosedata, kdes = self.get_dicts()
+            mindose = np.amin(self.logdoses)
+            maxdose = np.amax(self.logdoses)
+            mindoseparam = np.mean(dosedata[mindose])
+            maxdoseparam = np.mean(dosedata[maxdose])
+            sdparam = np.std(self.drugdf[self.fitparam])/5
+            if mindoseparam < maxdoseparam:
+                minparam = np.amin(dosedata[mindose])
+                maxparam = np.amax(dosedata[maxdose])
+                minrange = minparam - 2 * sdparam
+                maxrange = maxparam + 2 * sdparam
+            else:
+                minparam = np.amax(dosedata[mindose])
+                maxparam = np.amin(dosedata[maxdose])
+                minrange= maxparam - 2 * sdparam
+                maxrange = minparam + 2 * sdparam
+
+            bottom = pm.Lognormal('bottom', minparam, sdparam)
+            top = pm.Lognormal('top', maxparam, sdparam)
+            logIC50 = pm.Normal('logIC50', np.mean(self.logdoses), np.std(self.logdoses))
+            hillslope = pm.Normal('hillslope', 1, 0.5)
+            
+            # Set up data input for interpolated distribution
+            datarange = np.exp(np.arange(minrange, maxrange, (minrange-maxrange)/200))
+
+            for logdose in self.logdoses:
+                kde = kdes[logdose]
+                # Distribution
+                y = bottom + (top - bottom) / (1 + np.power(10., (logIC50-logdose)*hillslope))
+                pm.Interpolated('dis'+str(logdose), datarange, kde.pdf(datarange), observed = y)
+
+        return curve_model
+
+    def sampling(self):
+        self.model = self.build_model()
+        get_MAP = pm.find_MAP(model=self.model)
+        print(get_MAP)
+        with self.model:
+            self.samples = pm.sample(start = get_MAP)
+        return self.samples
--- grmodel/sampleAnalysis.py
+++ grmodel/sampleAnalysis.py
@@ -3,8 +3,11 @@ import os
 import pymc3 as pm
 import numpy as np
 import scipy as sp
+import pandas as pd
 import matplotlib
+import matplotlib.pyplot as plt 
 from matplotlib.backends import backend_pdf
+from .pymcGrowth import simulate
 
 try:
     import cPickle as pickle
@@ -16,7 +19,7 @@ def read_dataset(ff=None):
     ''' Read the specified column from the shared test file. '''
 
     if ff is None:
-        ff = "062117_PC9"
+        ff = "030317-2_H1299"
 
     filename = './grmodel/data/' + ff + '_samples.pkl'
 
@@ -25,10 +28,35 @@ def read_dataset(ff=None):
 
     return classList
 
+def readCols(cols, trim = True):
+    ''' Load specified columns '''
+    grCols = []
+    grdf = []
+    classList = read_dataset()
+    for ii, item in enumerate(classList):
+        if item.selCol in cols:
+            grCols.append(item)
+
+    for item in grCols:
+        df = pm.backends.tracetab.trace_to_dataframe(item.samples)
+        df['Column'] = item.selCol
+        df['Condition'] = item.condName
+
+        if trim:
+            cutoff = np.amin(df['ssqErr'])+50
+            df = df.loc[df['ssqErr'] < cutoff,:]
+        grdf.append(df)
+
+    grdf = pd.concat(grdf)
+
+    if len(cols) == 1:
+        grCols = grCols[0]
+
+    return (grCols, grdf)
 
 def diagnostics(classList, plott=False):
     """ Check the convergence and general posterior properties of a chain. """
-
+    flag = True
     # Iterate over sampling columns
     for ii, item in enumerate(classList):
 
@@ -43,39 +71,65 @@ def diagnostics(classList, plott=False):
 
         gewekeOut = 0.0
         gewekeNum = 0
+        gewekeDivnum = 0
+        gewekeDiv = []
+        gewekeDivparam = []
+        #Keeps track of diverging chains for each column
+        divparamnum = 0
+        divparam = []
 
         for key, value in geweke.items():
             for kk, vv in value.items():
                 Vec = np.absolute(vv[:, 1])
-
+                intervals = len(Vec)
+                VecDiv= [val for val in Vec if val >= 1]
+                divnum = len([val for val in Vec if val >= 1.96])
+                
+                gewekeDivnum += len(VecDiv)
+                if len(VecDiv) > 0:
+                    gewekeDiv.extend(VecDiv)
+                    gewekeDivparam.append(kk)
                 gewekeOut += np.sum(Vec)
                 gewekeNum += Vec.size
 
+                # Hypothesis testing for single chain
+                z = (divnum - intervals*0.05) / np.sqrt(intervals*0.05*0.95)
+                p_value = 1 - sp.stats.norm.cdf(z)
+                if p_value <= 0.05:
+                    divparamnum += 1
+                    divparam.append(kk)
+
         # Let the z-score surpass 1 up to three times, or fewer with higher deviation
         # TODO: Need to come up with a null model for Geweke to test for convergence
-        if False:
-            print('Not converged according to Geweke.')
-            print(sp.stats.chi2.cdf(gewekeOut, gewekeNum))
-            print(gewekeOut)
-            print(gewekeNum)
-            return False
+        if gewekeDivnum > 3:
+            print('Column ' + str(item.selCol) + ' sampling not converged according to Geweke.')
+            print('z-score surpassed 1 for ' + str(gewekeDivnum) + ' times for parameters ' + str(gewekeDivparam) + ': \n' + str(gewekeDiv)) 
+#            print('p-value = ' +str(1-sp.stats.chi2.cdf(gewekeOut, gewekeNum)))
+#            print('gewekeOut = ' + str(gewekeOut))
+#            print('gewekeNum = ' + str(gewekeNum))
+            if divparamnum > 0:
+                print('divparamnum = ' + str(divparamnum) + ' for param(s) ' + str(divparam))
+            print('\n')
+            flag = False
 
         if min(neff.values()) < 100:
-            print('Effective N of sampling is less than 100.')
+            print('Column ' + str(item.selCol) + ' effective N of sampling is less than 100.')
             print(neff)
-            return False
+            print('\n')
+            flag = False
 
         if max(gr.values()) > 1.1:
-            print('Gelman-Rubin statistic failed.')
+            print('Gelman-Rubin statistic failed for column ' + str(item.selCol))
             print(gr)
-            return False
+            print('\n')
+            flag = False
 
     # Only output the posterior plot if we've converged
-    if plott is True:
+    if plott and flag:
         saveplot(classList, pm.plot_posterior)
 
     # Made it to the end so consistent with converged
-    return True
+    return flag
 
 
 def saveplot(cols, func):
@@ -89,8 +143,155 @@ def saveplot(cols, func):
     with backend_pdf.PdfPages(filename, keep_empty=False) as pdf:
         # Output sampling for each column
         for col in cols:
-            fig, axis = matplotlib.pyplot.subplots(9, 2)
+            fig, axis = matplotlib.pyplot.subplots(9, 1)
             axis = func(col.samples, ax=axis)
             matplotlib.pyplot.tight_layout()
             pdf.savefig(fig)
             matplotlib.pyplot.close()
+
+
+def sim_plot(column, rep=None, data=None, printt=False, show=True, axis=None):
+    """ 
+    Given column, plots simulation of predictions overlaying observed data 
+    rep: number of replicates per column
+    printt: print dataset and dataset shape
+    show: show figure
+    axis: pass in figure axis
+    """
+    # Read in dataset to Pandas data frame
+    if data == None:
+        classM, pdset = readCols([column])
+    else:
+        classM, pdset = data
+
+    if printt:
+        print(pdset.shape)
+        print(pdset)
+
+    #Initialize variables 
+    if rep != None:
+        time = classM.timeV.reshape(rep, int(len(classM.timeV) / rep))[0, :]
+    else:
+        time = classM.timeV
+    calcset = np.full((pdset.shape[0], len(time)), np.inf)
+    calcseta = np.full((pdset.shape[0], len(time)), np.inf)
+    calcsetd = np.full((pdset.shape[0], len(time)), np.inf)
+
+    varr = 0
+    # Get indexes for params
+    idic = {}
+    for param in ['div', 'd', 'deathRate', 'apopfrac', 'confl_conv', 'apop_conv', 'dna_conv', 'std']:
+        idic[param] = pdset.columns.get_loc(param)
+    # Evaluate predictions for each set of parameter fits
+    for row in pdset.iterrows():
+        mparm = np.copy(np.array(row[1].as_matrix()[[idic['div'], idic['d'], idic['deathRate'], idic['apopfrac']]]))
+#        try:
+            # Use old_model to calculate lnum, eap, and dead over time
+        simret = simulate(mparm, time)
+        if rep:
+            simret = simret[:len(time), :]
+            simret = simret.reshape((len(time), rep))
+
+            # Calculate predictions for total, apop, and dead cells over time
+        calcset[varr, :] = np.sum(simret, axis=1)*row[1].as_matrix()[idic['confl_conv']]
+        calcseta[varr, :] = np.sum(simret[:, 1:3], axis=1)*row[1].as_matrix()[idic['apop_conv']]
+        calcsetd[varr, :] = simret[:, 2]*row[1].as_matrix()[idic['dna_conv']]
+
+        varr = varr + 1
+#        except:
+#            print('Failed')
+#            continue
+    
+    # Plot prediction distribution and observation
+    # Iterate over total, apop, and dead cels
+    if axis == None:
+        plt.figure()
+        axis = plt.gca()
+    colors = ['b', 'g', 'r']
+    calcsets = [calcset, calcseta, calcsetd]
+    for i in list(range(len(calcsets))):
+        calc = calcsets[i]
+        c = colors[i]
+        # Get median & 90% confidence interval for each time point
+        qqq = np.percentile(calc, [5, 25, 50, 75, 95], axis=0)
+        # Plot confidence interval 
+        axis.plot(time, qqq[2, :], color = c)
+        axis.fill_between(time, qqq[1, :], qqq[3, :], alpha=0.5)
+        axis.fill_between(time, qqq[0, :], qqq[4, :], alpha=0.2)
+    # Plot observation 
+    axis.scatter(classM.timeV, classM.expTable['confl'], color = 'b', marker = '.')
+    axis.scatter(classM.timeV, classM.expTable['apop'], color = 'g', marker = '.')
+    axis.scatter(classM.timeV, classM.expTable['dna'], color = 'r', marker = '.')
+
+    if show:
+        plt.show()
+        axis.set_xlabel('Time (hr)')
+        axis.set_ylabel('% Confluence')
+        axis.set_title('Sim_plot '+pdset['Condition'].as_matrix()[0])
+    else:
+        title = pdset['Condition'].as_matrix()[0]
+        return (axis, title)
+
+
+def sim_plots(columns=None, rep = None):
+    if columns is None:
+        columns = list(range(3,19))
+    f, ax = plt.subplots(len(columns)//4, 4, figsize = (9.2, 2.3*(len(columns)//4)), sharex=True, sharey= False)
+    for i in list(range(len(columns))):
+        axis, title = sim_plot(columns[i], show=False, axis = ax[i//4, i%4], rep = rep)
+        ax[i//4, i%4] = axis
+        ax[i//4, i%4].set_title(title)
+        if i%4 == 0:
+            ax[i//4, i%4].set_ylabel('% Confluence')
+        if i//4 == len(columns)//4-1:
+            ax[i//4, i%4].set_xlabel('Time (hr)')
+    plt.tight_layout()
+    plt.title('Sim_Plots', x = -3, y = 6)
+    plt.show()
+
+
+def fit_plot(param, column, replica = False):
+    '''
+    Inputs: param = a list of len(7) in normal space, column = column for corresponding observation
+    Plot model prediction overlaying observation
+    '''
+    # Import an instance of GrowthModel
+    classM, _ = readCols([column])
+
+    # Initialize variables and parameters 
+    if replica:
+        ltime = int(len(classM.timeV)/3)
+    else:
+        ltime = int(len(classM.timeV))
+    calcset = np.full((ltime), np.inf)
+    calcseta = np.full((ltime), np.inf)
+    calcsetd = np.full((ltime), np.inf)
+    mparm = param[0:4]
+
+    # Use old model to calculate cells nubmers
+    simret = classM.old_model(mparm, param[4], param[5], param[6])[1]
+    if replica:
+        simret = simret[:ltime,:]
+    simret = simret.reshape(ltime,3)
+
+    # Calculate total, apop, and dead cells 
+    calcset[:] = np.sum(simret,axis = 1) * param[4]
+    calcseta[:] = np.sum(simret[:,1:3], axis = 1) * param[5]
+    calcsetd[:] = simret[:,2] * param[6]
+    
+    # Plot prediction curves overlayed with observation 
+    if replica:
+        plt.plot(classM.timeV.reshape(3,ltime)[0,:], calcset)
+        plt.plot(classM.timeV.reshape(3,ltime)[0,:], calcseta)
+        plt.plot(classM.timeV.reshape(3,ltime)[0,:], calcsetd)
+    else:
+        plt.plot(classM.timeV, calcset)
+        plt.plot(classM.timeV, calcseta)
+        plt.plot(classM.timeV, calcsetd)
+    plt.scatter(classM.timeV, classM.expTable['confl'])
+    plt.scatter(classM.timeV, classM.expTable['apop'])
+    plt.scatter(classM.timeV, classM.expTable['dna'])
+    plt.title('Fit_plot '+str(column))
+    plt.xlabel('Time (hr)')
+    plt.ylabel('% Confluence')
+    plt.show()
