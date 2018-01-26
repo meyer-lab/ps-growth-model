64f221afec66e850d552570c91f6c4ad67d2b69c
Rui Yan <yanrui0498@gmail.com>
2018-01-25 18:58:32 -0800

    Code for Figure1

--- grmodel/figures/Figure1.py
+++ grmodel/figures/Figure1.py
@@ -4,6 +4,8 @@ This creates Figure 1.
 
 import pandas as pd
 import numpy as np
+import numpy_indexed as npi
+import scipy as sp
 import matplotlib
 import pymc3 as pm
 matplotlib.use('Agg')
@@ -16,46 +18,89 @@ from sklearn.preprocessing import StandardScaler
 from grmodel.fitcurves import sigmoid, residuals
 from ..pymcDoseResponse import loadCellTiter
 
+"""
+    Figure C: only the average of real data at each concentration X
+    Figure D: the average of lexp at each concentration X
+    Figure E: the average of expected number of dead cells at each X
+    Figrue F: no lines and only one color
+"""
 
-'''
-# Traceplot
-def traceplot(DoseResponseM, ax):
-    axx = pm.traceplot(DoseResponseM.samples, varnames='IC50', ax=ax)
-'''
 
-# Plot growth rate vs. death rate
-def GRvsDRFigureMaker(DoseResponseM, ax):
-    for i in np.random.choice(DoseResponseM.samples['growthV'].shape[0], 80):
-        axx = sns.regplot(DoseResponseM.samples['growthV'][i, :], DoseResponseM.samples['deathV'][i, :], lowess=True, ax=ax)
-        axx.set_xlim([0, 0.6])
-        axx.set_ylim([0, 0.6])
-        axx.set_xlabel('growthRate')
-        axx.set_ylabel('deathRate')
-        axx.set_title('growthRate vs. deathRate')
+# Print the average and 95 confidence interval
+def CIPlot(_x, _y, confidence, ax):
+    x_unique, y_mean = npi.group_by(_x).mean(_y)
+    y_std = npi.group_by(_x).std(_y)[1]
+    sample_size = npi.count(_x)[1]
+    yerr = []
+    for i in range(len(sample_size)):
+        yerr.append((y_std[i] / np.sqrt(sample_size)[i]) * sp.stats.t._ppf((1 + confidence) / 2., sample_size[i] - 1))
+    ax.errorbar(x=x_unique, y=y_mean, yerr=yerr, fmt='o')
+    ax.scatter(x_unique, y_mean)
+
+
+def RangePlot(_df, var, low, high, ax):
+    _x = _df['concentration'].tolist()
+    _y = _df[var].tolist()
+    x_unique, y_mean = npi.group_by(_x).mean(_y)
+    x_unq = _df.groupby('concentration')
+    y_low = np.array(x_unq.quantile(low)[var].tolist())
+    y_high = np.array(x_unq.quantile(high)[var].tolist())
+    ax.scatter(x_unique, y_mean)
+    ax.fill_between(x_unique, y_high, y_low, facecolor='blue', alpha=0.5)
 
 
 # Check that MCMC actually fit the data provided
-def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2):
-    """ Plot the curves for (lnum vs. X) """
-    # Compare the plots of (lObs vs. X and lExp vs. X)
-    # Using five sets of lExp values
-    for i in np.random.choice(DoseResponseM.samples['lExp'].shape[0], 5):
-        ax1.scatter(DoseResponseM.drugCs, DoseResponseM.samples['lExp'][i, :])
-
-    ax1.set_title('lnum vs. X')
-    ax1.set_xlabel('X')
+def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2, ax3, ax4):
+    """ Plot the curves for (lnum vs. X) and (dead vs. X)"""
+    # Plot lObs at each concentration X
+    X = np.array(DoseResponseM.drugCs)
+    lObs = np.array(DoseResponseM.lObs)
+    CIPlot(X, lObs, 0.95, ax1)
+    ax1.set_title('lObs vs. concentration')
+    ax1.set_xlabel('concentration')
     ax1.set_ylabel('the number of live cells')
 
-    # Using all sets of lExp values
-    for i in np.random.choice(DoseResponseM.samples['lExp'].shape[0], 200):
-        ax2.scatter(DoseResponseM.drugCs, DoseResponseM.samples['lExp'][i, :])
-
-    ax2.set_title('lnum vs. X')
-    ax2.set_xlabel('X')
+    # Using 1000 sets of lExp values
+    # Plot the average of lExp at each concentration X
+    for n, i in enumerate(np.random.choice(DoseResponseM.samples['lExp'].shape[0], 1000)):
+        lExp = np.array(DoseResponseM.samples['lExp'][i, :])
+        df1 = pd.DataFrame({'concentration': X, 'lExp': lExp})
+        if n == 0:
+            df2 = df1
+        else:
+            df2 = df2.append(df1, ignore_index=True)
+    ax2.set_title('lExp vs. concentration')
+    ax2.set_xlabel('concentration')
     ax2.set_ylabel('the number of live cells')
+    RangePlot(df2, 'lExp', 0.1, 0.9, ax2)
 
-    ax1.plot(DoseResponseM.drugCs, DoseResponseM.lObs, '^', color='black')
-    ax2.plot(DoseResponseM.drugCs, DoseResponseM.lObs, '^', color='black')
+    # Plot the DeadExp at each concentration X
+    for n, i in enumerate(np.random.choice(DoseResponseM.samples['growthV'].shape[0], 1000)):
+        GR = np.array(DoseResponseM.samples['growthV'][i, :])
+        DR = np.array(DoseResponseM.samples['deathV'][i, :])
+        df3 = pd.DataFrame({'concentration': X, 'growthRate': GR})
+        df4 = pd.DataFrame({'concentration': X, 'deathRate': DR})
+        if n == 0:
+            df5 = df3
+            df6 = df4
+        else:
+            df5 = df5.append(df3, ignore_index=True)
+            df6 = df6.append(df4, ignore_index=True)
+    RangePlot(df5, 'growthRate', ax3)
+    ax3.set_xlabel('concentration')
+    RangePlot(df6, 'deathRate', ax4)
+    ax4.set_xlabel('concentration')
+
+
+# Plot growth rate vs. death rate
+def GRvsDRFigureMaker(DoseResponseM, ax):
+    for i in np.random.choice(DoseResponseM.samples['growthV'].shape[0], 50):
+        ax.scatter(DoseResponseM.samples['growthV'][i, :], DoseResponseM.samples['deathV'][i, :], color='b')
+        ax.set_xlim([0, 0.6])
+        ax.set_ylim([0, 0.6])
+        ax.set_xlabel('growthRate')
+        ax.set_ylabel('deathRate')
+        ax.set_title('growthRate vs. deathRate')
 
 
 # Check the dimensionality of the sampling uncertainty using PCA
@@ -110,16 +155,13 @@ def makeFigure():
     # Get list of axis objects
     ax, f, gs1 = getSetup((7, 6), (3, 3))
 
-    GRvsDRFigureMaker(DoseResponseM, ax[5])
-    #traceplot(DoseResponseM, ax[5])
-    DataFitCheckFigureMaker(DoseResponseM, ax[6], ax[7])
-    PCAFigureMaker(DoseResponseM, ax[8])
+    DataFitCheckFigureMaker(DoseResponseM, ax[2], ax[3], ax[4], ax[5])
+    GRvsDRFigureMaker(DoseResponseM, ax[6])
+    PCAFigureMaker(DoseResponseM, ax[7])
 
     # Make first cartoon
     for ii, item in enumerate(ax):
         subplotLabel(item, ascii_uppercase[ii])
-
-    # doseResponseTiter(ax[1])
 
     # Try and fix overlapping elements
     f.tight_layout()
