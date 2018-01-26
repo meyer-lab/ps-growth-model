000af555434a23e181d271a1119c14c9711b61dd
Rui Yan <yanrui0498@gmail.com>
2018-01-26 10:48:53 -0800

    Revised figure 1 code

--- grmodel/figures/Figure1.py
+++ grmodel/figures/Figure1.py
@@ -24,33 +24,9 @@ from string import ascii_uppercase
 
 DoseResponseM = readSamples()
 
-"""
-    Figure C: only the average of real data at each concentration X
-    Figure D: the average of lexp at each concentration X
-    Figure E: the average of expected number of dead cells at each X
-    Figrue F: no lines and only one color
-"""
-
-'''
-def plotCurves(list, var, ax):
-    """ Plot the curves for (lnum vs. X, eap vs. X, dead vs. X) """
-    # list = [IC50s_fit, hill_fit, Emin_growth_fit, Emax_growth_fit, Emax_death_fit, time]
-    X = np.linspace(-1, 3)
-    drugTerm = 1.0 / (1.0 + np.power(10.0, (list[0] - X) * list[1]))
-    growthV = list[2] + (list[3] - list[2]) * drugTerm
-    deathV = list[4] * drugTerm
-    GR = growthV - deathV
-    lExp = np.exp(GR * list[5])
-    if var == 'lExp':
-        ax.scatter(X, lExp)
-    if var == 'growthV':
-        ax.scatter(X, growthV)
-    if var == 'deathV':
-        ax.scatter(X, deathV)
-'''
-
-
 # Plot the average and 95 confidence interval
+
+
 def CIPlot(_x, _y, confidence, ax):
     x_unique, y_mean = npi.group_by(_x).mean(_y)
     y_std = npi.group_by(_x).std(_y)[1]
@@ -58,8 +34,7 @@ def CIPlot(_x, _y, confidence, ax):
     yerr = []
     for i in range(len(sample_size)):
         yerr.append((y_std[i] / np.sqrt(sample_size)[i]) * sp.stats.t._ppf((1 + confidence) / 2., sample_size[i] - 1))
-    ax.errorbar(x=x_unique, y=y_mean, yerr=yerr, fmt='o')
-    ax.scatter(x_unique, y_mean)
+    ax.errorbar(x=x_unique, y=y_mean, yerr=yerr, fmt='o', color='black', alpha=0.5)
 
 
 # Plot the average and low and high quantile
@@ -71,11 +46,7 @@ def RangePlot(_df, var, low, high, ax):
     y_low = np.array(x_unq.quantile(low)[var].tolist())
     y_high = np.array(x_unq.quantile(high)[var].tolist())
     ax.scatter(x_unique, y_mean)
-    ax.fill_between(x_unique, y_high, y_low, facecolor='blue', alpha=0.5)
-
-
-# def getFitValue(var):
-#    return pm.stats.quantiles(DoseResponseM.samples[var], [50]).get(50)
+    ax.fill_between(x_unique, y_high, y_low, facecolor='b', alpha=0.5)
 
 
 def getListofValues(var):
@@ -94,11 +65,6 @@ def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2, ax3, ax4, ax5):
     ax1.set_ylabel('the number of live cells')
 
     # Plot the CI of lExp, growthV and deathV at each concentration X
-    #IC50s_fit = getFitValue('IC50s')
-    #hill_fit = getFitValue('hill')
-    #Emin_growth_fit = getFitValue('Emin_growth')
-    #Emax_growth_fit = getFitValue('Emax_growth')
-    #Emax_death_fit = getFitValue('Emax_death')
     IC50s = getListofValues('IC50s')
     hill = getListofValues('hill')
     Emin_growth = getListofValues('Emin_growth')
@@ -107,10 +73,12 @@ def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2, ax3, ax4, ax5):
 
     df = pd.DataFrame({'IC50s': IC50s, 'hill': hill, 'Emin_growth': Emin_growth,
                        'Emax_growth': Emax_growth, 'Emax_death': Emax_death})
+
     for n in range(len(df.index)):
-        x = np.linspace(-1, 3)
+        x = np.arange(-1.0, 3.0, 0.01)
         drugTerm = 1.0 / (1.0 + np.power(10.0, (df['IC50s'].iloc[n] - x) * df['hill'].iloc[n]))
-        growthV = df['Emin_growth'].iloc[n] + ((df['Emax_growth'].iloc[n] - df['Emin_growth'].iloc[n]) * drugTerm)
+        growthV = df['Emin_growth'].iloc[n] + ((df['Emax_growth'].iloc[n]
+                                                - df['Emin_growth'].iloc[n]) * drugTerm)
         deathV = df['Emax_death'].iloc[n] * drugTerm
         GR = growthV - deathV
         lnum = np.exp(GR * 72.0)
@@ -129,6 +97,7 @@ def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2, ax3, ax4, ax5):
             df6 = df6.append(df3, ignore_index=True)
 
     RangePlot(df4, 'lExp', 0.1, 0.9, ax2)
+    CIPlot(X, lObs, 0, ax2)  # Compare with the real data
     RangePlot(df5, 'growthV', 0.1, 0.9, ax3)
     RangePlot(df6, 'deathV', 0.1, 0.9, ax4)
 
@@ -136,13 +105,15 @@ def DataFitCheckFigureMaker(DoseResponseM, ax1, ax2, ax3, ax4, ax5):
     ax2.set_xlabel('concentration')
     ax2.set_ylabel('the number of live cells')
     ax3.set_xlabel('concentration')
+    ax3.set_ylabel('growthRate')
     ax4.set_xlabel('concentration')
+    ax4.set_ylabel('deathRate')
 
     # Plot growth rate vs. death rate
     for n in range(len(x)):
         ax5.scatter(df2['growthV'].iloc[n], df3['deathV'].iloc[n], color='b')
-        ax5.set_xlim([0, 0.1])
-        ax5.set_ylim([0, 0.05])
+        ax5.set_xlim([0, 0.05])
+        ax5.set_ylim([0, 0.02])
         ax5.set_xlabel('growthRate')
         ax5.set_ylabel('deathRate')
         ax5.set_title('growthRate vs. deathRate')
@@ -164,21 +135,21 @@ def PCAFigureMaker(self, ax):
 
     # print(pca.explained_variance_ratio_)
     # Scatter plot of PC1 vs. PC2
-    ax.scatter(X_new[:, 0], X_new[:, 1], c='aqua')
+    ax.scatter(X_new[:, 0], X_new[:, 1], alpha=0.5)
 
     # Plot the direction of each variable
     coeff = pca.components_
     n = coeff.shape[0]
     labels = ['IC50s', 'hill', 'Emin_growth', 'Emin_growth', 'Emax_death']
-    for i in range(n):
-        ax.arrow(0, 0, coeff[i, 0], coeff[i, 1], color='r', alpha=0.5)
-        ax.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15, labels[i],
-                color='b', ha='center', va='center')
+    # for i in range(n):
+    #    ax.arrow(0, 0, coeff[i, 0], coeff[i, 1], color='black')
+    #    ax.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15, labels[i],
+    #            color='black', ha='center', va='center')
 
     ax.set_xlabel("PC{}".format(1))
     ax.set_ylabel("PC{}".format(2))
     ax.set_title("2 components PCA")
-    ax.grid()
+    ax.grid(True)
 
 
 def makeFigure():
