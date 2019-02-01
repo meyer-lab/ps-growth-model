c470fc780e3e01f1aed36b93ce42dbc506aa1619
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-02-01 12:45:53 -0800

    New cartoons from SY (#104)
    * Add files via upload
    
    * Delete Figure2-Schematic.svg
    
    * Change sizing.

--- genFigures.py
+++ genFigures.py
@@ -17,9 +17,13 @@ if __name__ == '__main__':
 
     ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
 
-    if sys.argv[1] == '2':
+    if sys.argv[1] == '1':
+        # Overlay Figure 2 cartoon
+        overlayCartoon(fdir + 'Figure1.svg',
+                       './grmodel/figures/Figure1-Schematic.svg', 23, 14, 0.4)
+    elif sys.argv[1] == '2':
         # Overlay Figure 2 cartoon
         overlayCartoon(fdir + 'Figure2.svg',
-                       './grmodel/figures/Figure2-Schematic.svg', 23, 4, 0.094)
+                       './grmodel/figures/Figure2-Schematic.svg', 23, 4, 0.4)
 
     print(nameOut + ' is done.')
